8edd4e3a33984054f043bd54549447c2994fbb20
VinlaRose <vinla.files@gmail.com>
2023-07-09 20:15:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 
 import './App.css';
-import { ImageCard } from './components/ImageCard/ImageCards';
-import { Card } from './components/card/card';
-import { InfoCard } from './components/infoCard/infoCard';
-import { NavBar } from './components/navCard/navCard';
-import { PriceCard } from './components/priceCard/priceCard';
 
 function App() {
   return (
     <div className="App">
-     <NavBar className="nav-bar" />
-     <br/>
-     <PriceCard/>
-     <br/>
-     <Card/>
-     <br/>
-     <InfoCard/>
-     <br/>
-     <ImageCard/>
+      hi
+    
     </div>
   );
 }
